3581e6cbd98a5156064fcd7d5f52ca4846873405
MaximShtaba <111359641+MaximShtaba@users.noreply.github.com>
2023-01-17 23:33:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+	let str = '';
+	let i = 0;
+	let toStr = String(Math.abs(n));
+	while (i<toStr.length) {
+	  str = toStr[i] + str;
+	  i= i + 1;
+	}
+	return Number(str);
 }
